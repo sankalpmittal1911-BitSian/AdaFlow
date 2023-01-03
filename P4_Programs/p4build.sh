3c4fddf32638935c7b0bfffdd7d185f4946d270d
Sankalp-CS21MTECH12010 <104780009+Sankalp-CS21MTECH12010@users.noreply.github.com>
2023-01-03 17:12:06 +0530

    Update p4build.sh

--- P4_Programs/p4build.sh
+++ P4_Programs/p4build.sh
@@ -1,1 +1,4 @@
-
+$SDE_INSTALL/bin/bf-p4c AdaFlow_11Features.p4
+cmake $SDE/p4studio -DCMAKE_MODULE_PATH="$SDE/cmake" -DCMAKE_INSTALL_PREFIX="$SDE_INSTALL" -DP4_PATH="/home/c310/P4-Project/AdaFlow_11Features.p4" -DP4_NAME="AdaFlow_11Features" -DP4_LANG="p4_16" -DTOFINO=ON -DTOFINO2=OFF
+make
+make install
